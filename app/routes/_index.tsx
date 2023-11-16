5fc8e2f541ce0ab6343f86f890064f2353003b88
Phúc Nguyễn <40336551+phucnguyen035@users.noreply.github.com>
2023-11-16 14:43:15 +0700

    move login form to component

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,51 +1,45 @@
 import {
-  json,
-  type ActionFunctionArgs,
-  type LoaderFunctionArgs,
-} from "@remix-run/cloudflare";
-import { Form, useActionData, useLoaderData } from "@remix-run/react";
-import bcrypt from "bcryptjs";
+	json,
+	redirect,
+	type ActionFunctionArgs,
+	type LoaderFunctionArgs,
+} from '@remix-run/cloudflare';
+import { Form, useActionData, useLoaderData } from '@remix-run/react';
 
+import LoginForm from '~/components/LoginForm';
 export function meta() {
-  return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
-  ];
+	return [{ title: 'New Remix App' }, { name: 'description', content: 'Welcome to Remix!' }];
 }
 
 export async function loader({ context }: LoaderFunctionArgs) {
-  return json({ users: await context.db.query.users.findMany() });
+	return json({ users: await context.db.query.users.findMany() });
 }
 
 export async function action({ request }: ActionFunctionArgs) {
-  const formData = await request.formData();
-  const password = formData.get("password")?.toString() ?? "";
-  if (!password) {
-    throw new Error("Password is required");
-  }
+	const formData = await request.formData();
+	const password = formData.get('password')?.toString() ?? '';
+	if (!password) {
+		return json({ errors: { password: 'Password is required' } });
+	}
 
-  return await bcrypt.hash(password, 10);
+	return redirect('/');
 }
 
 export default function Index() {
-  const { users } = useLoaderData<typeof loader>();
-  const hashed = useActionData<typeof action>();
+	const { users } = useLoaderData<typeof loader>();
+	const data = useActionData<typeof action>();
 
-  return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <Form method="post">
-        <input type="email" placeholder="john.smith@example.com" name="email" />
-        <input type="password" name="password" />
-        <button>Sign up</button>
-      </Form>
-      {hashed && <p>{hashed}</p>}
+	return (
+		<main>
+			<Form method="POST">
+				<LoginForm errors={data?.errors} />
+			</Form>
 
-      <ul>
-        {users.map((user) => (
-          <li key={user.id}>{user.password}</li>
-        ))}
-      </ul>
-    </div>
-  );
+			<ul>
+				{users.map((user) => (
+					<li key={user.id}>{user.password}</li>
+				))}
+			</ul>
+		</main>
+	);
 }
