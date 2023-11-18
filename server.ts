e0db4f17c2fdc993969277c934ed0304306b3498
Phúc Nguyễn <40336551+phucnguyen035@users.noreply.github.com>
2023-11-18 20:29:39 +0700

    add sessions

--- server.ts
+++ server.ts
@@ -1,23 +1,46 @@
-import { logDevReady } from "@remix-run/cloudflare";
-import { createPagesFunctionHandler } from "@remix-run/cloudflare-pages";
-import * as build from "@remix-run/dev/server-build";
-import { createDb } from "~/db.server";
+import { createCookie, createWorkersKVSessionStorage, logDevReady } from '@remix-run/cloudflare';
+import { createPagesFunctionHandler } from '@remix-run/cloudflare-pages';
+import * as build from '@remix-run/dev/server-build';
+import type { DrizzleD1Database } from 'drizzle-orm/d1';
+import { drizzle } from 'drizzle-orm/d1';
+import { schemas } from '~/schemas';
 
-declare module "@remix-run/cloudflare" {
-  interface AppLoadContext {
-    db: ReturnType<typeof createDb>;
-  }
+declare module '@remix-run/cloudflare' {
+	interface AppLoadContext {
+		db: DrizzleD1Database<typeof schemas>;
+		sessions: ReturnType<typeof createWorkersKVSessionStorage<{ userId: number }>>;
+	}
 }
 
-if (process.env.NODE_ENV === "development") {
-  logDevReady(build);
+interface ENV {
+	SESSION_SECRET: string;
+	DB: D1Database;
+	KV_FM_LINK_SHARING: KVNamespace;
+}
+
+if (process.env.NODE_ENV === 'development') {
+	logDevReady(build);
 }
 
 export const onRequest = createPagesFunctionHandler({
-  build,
-  getLoadContext: (context) => ({
-    env: context.env,
-    db: createDb(context.env.DB),
-  }),
-  mode: build.mode,
+	build,
+	getLoadContext: (context) => {
+		const env = context.env as ENV;
+		return {
+			env,
+			db: drizzle(env.DB, {
+				schema: schemas,
+				logger: process.env.NODE_ENV === 'development',
+			}),
+			sessions: createWorkersKVSessionStorage({
+				kv: env.KV_FM_LINK_SHARING,
+				cookie: createCookie('__session', {
+					httpOnly: true,
+					secure: process.env.NODE_ENV === 'production',
+					secrets: [env.SESSION_SECRET],
+				}),
+			}),
+		};
+	},
+	mode: build.mode,
 });
