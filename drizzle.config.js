e0d567658d8d41eb3ef21b801375b0c9e9c6c8eb
Phúc Nguyễn <40336551+phucnguyen035@users.noreply.github.com>
2023-11-18 20:44:38 +0700

    add drizzle studio config

--- drizzle.config.js
+++ drizzle.config.js
@@ -1,6 +1,13 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 /** @type {import('drizzle-kit').Config} */
 export default {
-  driver: "d1",
-  schema: "./app/schemas/*",
-  out: "./drizzle",
+	driver: 'd1',
+	schema: './app/schemas/*',
+	out: './drizzle',
+	dbCredentials: {
+		wranglerConfigPath: './wrangler.toml',
+		dbName: 'fm_link_sharing',
+	},
 };
