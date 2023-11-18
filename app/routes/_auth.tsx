950d595de8438fe2b556dfb69e80c940ddd3913a
Phúc Nguyễn <40336551+phucnguyen035@users.noreply.github.com>
2023-11-18 21:23:23 +0700

    add auth layout

---
+++ app/routes/_auth.tsx
@@ -0,0 +1,21 @@
+import { redirect, type LoaderFunctionArgs } from '@remix-run/cloudflare';
+import { Outlet } from '@remix-run/react';
+
+export async function loader({ request, context }: LoaderFunctionArgs) {
+	const session = await context.sessions.getSession(request.headers.get('Cookie'));
+	if (session.has('userId')) {
+		return redirect('/');
+	}
+
+	return null;
+}
+
+export default function AuthLayout() {
+	return (
+		<main className="grid min-h-screen place-items-center bg-muted">
+			<div className="w-full max-w-md">
+				<Outlet />
+			</div>
+		</main>
+	);
+}
