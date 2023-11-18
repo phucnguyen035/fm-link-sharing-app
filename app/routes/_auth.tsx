85e9bd813911a25b49f15ae12c7c3cebdae1ab77
Phúc Nguyễn <40336551+phucnguyen035@users.noreply.github.com>
2023-11-18 22:56:22 +0700

    add logo component

--- app/routes/_auth.tsx
+++ app/routes/_auth.tsx
@@ -1,5 +1,6 @@
 import { redirect, type LoaderFunctionArgs } from '@remix-run/cloudflare';
 import { Outlet } from '@remix-run/react';
+import Logo from '~/components/Logo';
 
 export async function loader({ request, context }: LoaderFunctionArgs) {
 	const session = await context.sessions.getSession(request.headers.get('Cookie'));
@@ -13,7 +14,10 @@ export async function loader({ request, context }: LoaderFunctionArgs) {
 export default function AuthLayout() {
 	return (
 		<main className="grid h-screen place-items-center">
-			<div className="w-full max-w-md">
+			<div className="grid w-full max-w-md gap-y-12">
+				<div className="flex justify-center">
+					<Logo />
+				</div>
 				<Outlet />
 			</div>
 		</main>
